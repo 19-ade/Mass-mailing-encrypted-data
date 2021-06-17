b6d11649c9eb1fc7a2e7e2add83e59f069af9aee
19-ade <anwesan4github@gmail.com>
2021-06-17 17:38:27 +0530

    adding a rough database

---
+++ database_manage.py
@@ -0,0 +1,88 @@
+import sqlite3
+
+# creating the database
+conn = sqlite3.connect('mass_mailing.db')
+
+# create a cursor for database
+cursor = conn.cursor()
+
+
+# create table
+
+def creation():
+    cursor.execute(" CREATE TABLE mail_list(\n"
+
+                   "    Name text,\n"
+                   "    Email text,\n"
+                   "    DOB integer\n"
+                   "   )")
+
+
+# NULL, INTEGER, REAL , TEXT, BLOB(IMAGES, MP3 FILE)
+# cursor.fetchall() to return all the queries in a list
+
+
+def input_(name, email, dob):
+    cursor.execute("INSERT INTO mail_list VALUES (?, ?, ?)", [name, email, dob])
+    conn.commit()
+
+
+def delete_(query, column):
+    if column == 'DOB':
+
+        cursor.execute("DELETE FROM mail_list WHERE DOB = ? ", (query,))
+    elif column == 'Email':
+        cursor.execute("DELETE FROM mail_list WHERE Email = ? ", (query,))
+    conn.commit()
+
+
+def get_query():
+    cursor.execute("SELECT * FROM mail_list")
+    l = cursor.fetchall()
+
+    return l
+
+def reset():
+    cursor.execute("DELETE FROM mail_list")
+    conn.commit()
+
+def initialize():
+    array = [
+        ("Anwesan de",'f20190518@hyderabad.bits-pilani.ac.in', 19092000),
+        ("Aditya Goyal", 'wdenny@gmail.com', 12042001),
+        ("Abhinav gupta", 'f20190478@hyderabad.bits-pilani.ac.in', 16012000),
+        ('Nirav jayesh parmar', 'f20190540@hyderabad.bits-pilani.ac.in', 7022002)
+
+    ]
+    cursor.executemany("INSERT INTO mail_list VALUES (?, ?, ?)", array)
+    conn.commit()
+
+#reset()
+#creation()
+#initialize()
+#print(get_query())
+'''
+while 1:
+    a, b, d = input().split()
+    input_(a, b, d)
+
+    e = int(input("Wnat to add more 1 0r 0"))
+    if e:
+        continue
+    else:
+        print("delete shit?")
+        a = int(input())
+        if a:
+            while 1:
+                q, c = input().split()
+                print(q, c)
+                delete_(q, c)
+                d = int(input("still wanna?"))
+                if d:
+                    continue
+                else:
+                    break
+
+        get_query()
+        break
+'''
