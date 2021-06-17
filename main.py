bd66c9cf4d15cce906a48984f77f0d7672636a63
19-ade <anwesan4github@gmail.com>
2021-06-17 13:28:56 +0530

    minor changes

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ window.config(padx=75, pady=50, bg="white")
 
 
 canvas = Canvas(width=200, height=200, bg="white", highlightthickness=0)
-logo_img = PhotoImage(file="images/logo.png")
+logo_img = PhotoImage(file="logo.png")
 canvas.create_image(152, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
