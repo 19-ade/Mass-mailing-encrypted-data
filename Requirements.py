c9b5fb8440131e5ce845f3a2e0a843db23ad3cd1
19-ade <anwesan4github@gmail.com>
2021-06-18 16:16:43 +0530

    Run this to install libraries

---
+++ Requirements.py
@@ -0,0 +1,15 @@
+import os
+import subprocess
+import sys
+
+
+def install(package):
+    os.system("pip install " + str(package))
+    reqs = subprocess.check_output([sys.executable, "-m", "pip", "show", str(package)])
+
+    print(str(reqs) + "\n")
+    print("Installed " + package.upper() + "\n")
+
+
+install("secure-smtplib")
+install("PyPDF2")
